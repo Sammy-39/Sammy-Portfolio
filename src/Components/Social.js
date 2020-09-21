e5b580320171f2af414421746d82e4cd9929658a
Sammy <o.samy39@gmail.com>
2020-09-22 01:52:39 +0530

    Fixed icons not rendering problem

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -5,12 +5,14 @@ class Social extends Component {
         return (
         <div className="social">
 
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
             <a href="https://github.com/Sammy-39" target="_blank">
-                <i class="fab fa-github" /> Github
+                <i class="fa fa-github"></i>
             </a>
-            
+
             <a href="https://www.linkedin.com/in/madasamy-pandiarajan-910973197/" target="_blank">
-                <i class="fab fa-linkedin-in" /> LinkedIN
+                <i class="fa fa-linkedin"></i>
             </a>
             
         </div>
